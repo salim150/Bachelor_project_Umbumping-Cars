196ed5cb133f276a745221cbd46f43ca7efdbdb6
Giacomo Burani <buranig@stuent.ethz.ch>
2023-11-22 11:34:30 +0100

    Now the plotter works with a timer

--- src/bumper_cars/bumper_cars/plotter.py
+++ src/bumper_cars/bumper_cars/plotter.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 import math
 from matplotlib import pyplot as plt
-from custom_message.msg import FullState, Path, MultiplePaths, MultiState
+from custom_message.msg import FullState, Path, MultiplePaths, MultiState, Coordinate
 import message_filters
 import numpy as np
 import time
@@ -26,7 +26,7 @@ safety = json_object["safety"]
 width = json_object["width"]
 height = json_object["height"]
 debug = False
-plot_traj = False
+plot_traj = True
 
 class Config:
     """
@@ -47,6 +47,34 @@ class Plotter(Node):
     def __init__(self):
         super().__init__("plotter")
 
+        self.declare_parameters(
+            namespace='',
+            parameters=[
+                ('model_type', rclpy.Parameter.Type.STRING_ARRAY),
+                ('x0', rclpy.Parameter.Type.DOUBLE_ARRAY),
+                ('y0', rclpy.Parameter.Type.DOUBLE_ARRAY),
+                ('yaw', rclpy.Parameter.Type.DOUBLE_ARRAY),
+                ('v', rclpy.Parameter.Type.DOUBLE_ARRAY),
+                ('omega', rclpy.Parameter.Type.DOUBLE_ARRAY)
+            ]
+        )
+
+        x0 = self.get_parameter('x0').get_parameter_value().double_array_value
+        y0 = self.get_parameter('y0').get_parameter_value().double_array_value
+        yaw = self.get_parameter('yaw').get_parameter_value().double_array_value
+        v = self.get_parameter('v').get_parameter_value().double_array_value
+        omega = self.get_parameter('omega').get_parameter_value().double_array_value
+        model_type = self.get_parameter('model_type').get_parameter_value().string_array_value
+
+        self.multi_state = MultiState()
+        self.multi_traj = MultiplePaths()
+
+        # Initializing the robots TODO add in the launch file parameters the initial control--> here it's hardcoded
+        for i in range(robot_num):
+            self.multi_state.multiple_state.append(FullState(x=x0[i], y=y0[i], yaw=yaw[i], v=v[i],
+                                                             omega=omega[i], delta=0.0, throttle=0.0))
+            self.multi_traj.multiple_path.append(Path(path=[Coordinate(x=0.0, y=0.0)]))
+
         multi_state_sub = message_filters.Subscriber(self, MultiState, "/multi_fullstate")
         self.multi_path_sub = message_filters.Subscriber(self, MultiplePaths, "/robot_multi_traj")
 
@@ -59,21 +87,40 @@ class Plotter(Node):
         self.heigth = height
 
         if self.controller_type == "random_walk":
+            # self.multi_state = MultiState()
             ts = message_filters.ApproximateTimeSynchronizer([multi_state_sub], 10, 1, allow_headerless=True)
-            ts.registerCallback(self.plotter_callback)
+            ts.registerCallback(self.general_callback)
+
+            # timer
+            self.timer = self.create_timer(0.01, self.plotter_callback)
 
         elif self.controller_type == "random_harem":
+            # self.multi_state = MultiState()
             ts = message_filters.ApproximateTimeSynchronizer([multi_state_sub], 10, 1, allow_headerless=True)
-            ts.registerCallback(self.plotter_callback)
+            ts.registerCallback(self.general_callback)
+
+            # timer
+            self.timer = self.create_timer(0.01, self.plotter_callback)
             
         else:
+            # self.multi_state = MultiState()
+            # self.multi_traj = MultiplePaths()
             ts = message_filters.ApproximateTimeSynchronizer([multi_state_sub, self.multi_path_sub], 10, 1, allow_headerless=True)
-            ts.registerCallback(self.complete_plotter_callback)
+            ts.registerCallback(self.complete_callback)
+
+            # timer
+            self.timer = self.create_timer(0.01, self.complete_plotter_callback)
 
         self.get_logger().info("Plotter has been started")
 
-    def plotter_callback(self, multi_state: MultiState):
-        
+    def general_callback(self, multi_state: MultiState):
+        self.multi_state = multi_state
+
+    def complete_callback(self, multi_state: MultiState, multi_traj: MultiplePaths):
+        self.multi_state = multi_state
+        self.multi_traj = multi_traj
+
+    def plotter_callback(self):        
         plt.cla()
         # for stopping simulation with the esc key.
         plt.gcf().canvas.mpl_connect(
@@ -81,23 +128,23 @@ class Plotter(Node):
             lambda event: [exit(0) if event.key == 'escape' else None])
 
         for i in range(robot_num):
-            self.plot_robot(multi_state.multiple_state[i])
+            self.plot_robot(self.multi_state.multiple_state[i])
             if debug:
-                self.get_logger().info("robot" + str(i) + ", x: " + str(multi_state.multiple_state[i].x) + ", " +
-                                    "y: " + str(multi_state.multiple_state[i].y) + ", " +
-                                    "yaw: " + str(multi_state.multiple_state[i].yaw) + ", " +
-                                    "linear velocity: " + str(multi_state.multiple_state[i].v))
+                self.get_logger().info("robot" + str(i) + ", x: " + str(self.multi_state.multiple_state[i].x) + ", " +
+                                    "y: " + str(self.multi_state.multiple_state[i].y) + ", " +
+                                    "yaw: " + str(self.multi_state.multiple_state[i].yaw) + ", " +
+                                    "linear velocity: " + str(self.multi_state.multiple_state[i].v))
 
         self.plot_map()
         plt.axis("equal")
         plt.pause(0.000001)
 
-    def complete_plotter_callback(self, multi_state: MultiState, multi_traj: MultiplePaths):
+    def complete_plotter_callback(self):
 
         buf = np.zeros((robot_num, 2))
         for i in range(robot_num):
-            buf[i, 0] = multi_state.multiple_state[i].x
-            buf[i, 1] = multi_state.multiple_state[i].y
+            buf[i, 0] = self.multi_state.multiple_state[i].x
+            buf[i, 1] = self.multi_state.multiple_state[i].y
 
         self.states_x_buf = np.concatenate((self.states_x_buf, buf[:,0].reshape(robot_num,1)), axis=1)
         self.states_y_buf = np.concatenate((self.states_y_buf, buf[:,1].reshape(robot_num,1)), axis=1)
@@ -114,13 +161,13 @@ class Plotter(Node):
         
         for i in range(robot_num):
 
-            self.plot_situation(multi_state.multiple_state[i], multi_traj.multiple_path[i], 
+            self.plot_situation(self.multi_state.multiple_state[i], self.multi_traj.multiple_path[i], 
                                 self.states_x_buf[i, :], self.states_y_buf[i, :])
             if debug:
-                self.get_logger().info("robot" + str(i) + ", x: " + str(multi_state.multiple_state[i].x) + ", " +
-                                    "y: " + str(multi_state.multiple_state[i].y) + ", " +
-                                    "yaw: " + str(multi_state.multiple_state[i].yaw) + ", " +
-                                    "linear velocity: " + str(multi_state.multiple_state[i].v))
+                self.get_logger().info("robot" + str(i) + ", x: " + str(self.multi_state.multiple_state[i].x) + ", " +
+                                    "y: " + str(self.multi_state.multiple_state[i].y) + ", " +
+                                    "yaw: " + str(self.multi_state.multiple_state[i].yaw) + ", " +
+                                    "linear velocity: " + str(self.multi_state.multiple_state[i].v))
 
         self.plot_map()
         plt.axis("equal")
