bc150c9a1eccfa049be95e753b2cf3b41ab45277
Giacomo Burani <buranig@stuent.ethz.ch>
2024-01-12 18:56:27 +0100

    Started with the code maintentance for the LBP method. I created a main2() function and various subfunctions to make the main() function code more readable.

--- src/lattice_planning/LBP.py
+++ src/lattice_planning/LBP.py
@@ -60,14 +60,6 @@ v_ref = 2.0 # [m/s] reference speed
 with open('/home/giacomo/thesis_ws/src/lattice_planning/LBP.json', 'r') as file:
     data = json.load(file)
 
-def lbp_control(x, goal, ob):
-    """
-    Dynamic Window Approach control
-    """
-    dw = calc_dynamic_window(x)
-    u, trajectory = calc_control_and_trajectory(x, dw, goal, ob)
-    return u, trajectory
-
 class RobotType(Enum):
     circle = 0
     rectangle = 1
@@ -114,34 +106,24 @@ def find_nearest(array, value):
     idx = (np.abs(array - value)).argmin()
     return array[idx]
 
+def lbp_control(x, goal, ob):
+    """
+    Dynamic Window Approach control
+    """
+    dw = calc_dynamic_window(x)
+    u, trajectory = calc_control_and_trajectory(x, dw, goal, ob)
+    return u, trajectory
+
 def calc_dynamic_window(x):
     """
     calculation dynamic window based on current state x
     motion model
     initial state [x(m), y(m), yaw(rad), v(m/s), delta(rad)]
     """
-    # Dynamic window from robot specification
-    # Vs = [min_speed, max_speed,
-    #       -max_steer, max_steer]
-    
-    
-    # # Dynamic window from motion model
-    # Vd = [x[3] - config.max_acc*0.1,
-    #       x[3] + config.max_acc*0.1,
-    #       -max_steer,
-    #       max_steer]
-    
-    # #  [min_throttle, max_throttle, min_steer, max_steer]
-    # dw = [min(Vs[0], Vd[0]), min(Vs[1], Vd[1]), min(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
-    
     Vs = [min_acc, max_acc,
           -max_steer, max_steer]
     
-    # Vd = [(min_speed - x[3])/0.1 , (max_speed - x[3])/0.1,
-    #       -max_steer, max_steer]
-    
     dw = [Vs[0], Vs[1], Vs[2], Vs[3]]
-    # dw = [max(Vs[0], Vd[0]), min(Vs[1], Vd[1]), max(Vs[2], Vd[2]), min(Vs[3], Vd[3])]
     return dw
 
 def predict_trajectory(x_init, a, delta):
@@ -163,35 +145,18 @@ def calc_control_and_trajectory(x, dw, goal, ob):
     """
     calculation final input with dynamic window
     """
-    x_init = x[:]
     min_cost = float("inf")
     best_u = [0.0, 0.0]
     best_trajectory = np.array([x])
 
-    # evaluate all trajectory with sampled input in dynamic window
-    # old_time = time.time()
-
-    # for id, info in data.items():
-    # print("\nV:", id)
-    # for key1, info1 in info.items():
-    #     print(f'\nAcc: {key1}')
-    #     for key2, info2 in info1.items():
-    #         print(f'delta: {key2}')
-    #         print(info2)
-    nearest = find_nearest(np.arange(min_speed, max_speed, v_resolution), x[3])
-    # previous_u.pop(0)
     a = (v_ref-x[3])/dt
     
-
     for id, info in data.items():
-
-        # old_time = time.time()
         geom = np.zeros((len(info['x']),3))
         geom[:,0] = info['x']
         geom[:,1] = info['y']
         geom[:,2] = info['yaw']
         geom[:,0:2] = (geom[:,0:2]) @ rotateMatrix(-x[2]) + [x[0],x[1]]
-        # print(time.time()-old_time)
 
         # trajectory = predict_trajectory(x_init, a, delta)
         trajectory = geom
@@ -206,9 +171,6 @@ def calc_control_and_trajectory(x, dw, goal, ob):
         # search minimum trajectory
         if min_cost >= final_cost:
             min_cost = final_cost
-
-            # interpolate the control inputs
-
             best_u = [a, info['ctrl'][2]]
             best_trajectory = trajectory
     return best_u, best_trajectory #, previous_u, previous_cost
@@ -277,7 +239,6 @@ def plot_robot(x, y, yaw):  # pragma: no cover
                         np.array([np.cos(yaw), np.sin(yaw)]) * robot_radius)
         plt.plot([x, out_x], [y, out_y], "-k")
 
-
 def main():
     print(__file__ + " start!!")
     # initial state [x(m), y(m), yaw(rad), v(m/s), omega(rad/s)]
@@ -311,9 +272,6 @@ def main():
             for idx in range(N):
                 if idx == i:
                     continue
-                # point = Point(x[0, idx], x[1, idx])
-                # point = point.buffer(dilation_factor, cap_style=3)
-                # ob.append(point)
                 if utils.dist([x1[0], x1[1]], [x[0, idx], x[1, idx]]) < WB: raise Exception('Collision')
                 ob.append(dilated_traj[idx])
             
@@ -326,7 +284,7 @@ def main():
             x[:, i] = x1
             u[:, i] = u1
             predicted_trajectory[i] = predicted_trajectory1
-            print(f'Robot {i} v: {x1[3]}')
+            # print(f'Robot {i} v: {x1[3]}')
 
         trajectory = np.dstack([trajectory, x])
         
@@ -466,9 +424,112 @@ def main1():
             plt.plot(trajectory[0,i,:], trajectory[1,i,:], "-r")
         plt.pause(0.0001)
         plt.show()
+
+def update_targets(paths, targets, x, i):
+    if utils.dist(point1=(x[0, i], x[1, i]), point2=targets[i]) < 5:
+        paths[i] = utils.update_path(paths[i])
+        targets[i] = (paths[i][0].x, paths[i][0].y)
+
+    return paths, targets
+
+def initialize_paths_targets_dilated_traj(x):
+    paths = []
+    targets = []
+    dilated_traj = []
+
+    for i in range(robot_num):
+        dilated_traj.append(Point(x[0, i], x[1, i]).buffer(dilation_factor, cap_style=3))
+        paths.append(utils.create_path())
+        targets.append([paths[i][0].x, paths[i][0].y])
+
+    return paths, targets, dilated_traj
+
+def update_robot_state(x, u, dt, targets, dilated_traj, predicted_trajectory, i):
+    x1 = x[:, i]
+    ob = [dilated_traj[idx] for idx in range(len(dilated_traj)) if idx != i]
+    u1, predicted_trajectory1 = lbp_control(x1, targets[i], ob)
+    dilated_traj[i] = LineString(zip(predicted_trajectory1[:, 0], predicted_trajectory1[:, 1])).buffer(dilation_factor, cap_style=3)
+    
+    # Collision check
+    if any([utils.dist([x1[0], x1[1]], [x[0, idx], x[1, idx]]) < WB for idx in range(robot_num) if idx != i]): raise Exception('Collision')
+    
+    x1 = motion(x1, u1, dt)
+    x[:, i] = x1
+    u[:, i] = u1
+    predicted_trajectory[i] = predicted_trajectory1
+
+    return x, u, predicted_trajectory
+
+def plot_robot_trajectory(x, u, predicted_trajectory, dilated_traj, targets, ax, i):
+    plt.plot(predicted_trajectory[i][:, 0], predicted_trajectory[i][:, 1], "-g")
+    plot_polygon(dilated_traj[i], ax=ax, add_points=False, alpha=0.5)
+    plt.plot(x[0, i], x[1, i], "xr")
+    plt.plot(targets[i][0], targets[i][1], "xg")
+    plot_robot(x[0, i], x[1, i], x[2, i])
+    plot_arrow(x[0, i], x[1, i], x[2, i], length=1, width=0.5)
+    plot_arrow(x[0, i], x[1, i], x[2, i] + u[1, i], length=3, width=0.5)
+
+def check_goal_reached(x, targets, i):
+    dist_to_goal = math.hypot(x[0, i] - targets[i][0], x[1, i] - targets[i][1])
+    if dist_to_goal <= 0.5:
+        print("Goal!!")
+        return True
+    return False
+
+def main2():
+    print(__file__ + " start!!")
+    iterations = 3000
+    break_flag = False
+
+    x, y, yaw, v, omega, model_type = utils.samplegrid(width_init, height_init, min_dist, robot_num, safety_init)
+    x = np.array([x, y, yaw, v])
+    u = np.zeros((2, robot_num))
+
+    trajectory = np.zeros((x.shape[0], robot_num, 1))
+    trajectory[:, :, 0] = x
+
+    predicted_trajectory = {}
+
+    paths, targets, dilated_traj = initialize_paths_targets_dilated_traj(x)
+
+    fig = plt.figure(1, dpi=90)
+    ax = fig.add_subplot(111)
+
+    for z in range(iterations):
+        plt.cla()
+        plt.gcf().canvas.mpl_connect('key_release_event', lambda event: [exit(0) if event.key == 'escape' else None])
+        
+        for i in range(robot_num):
+            
+            paths, targets = update_targets(paths, targets, x, i)
+
+            x, u, predicted_trajectory = update_robot_state(x, u, dt, targets, dilated_traj, predicted_trajectory, i)
+
+            trajectory = np.dstack([trajectory, x])
+
+            if check_goal_reached(x, targets, i):
+                break_flag = True
+
+            if show_animation:
+                plot_robot_trajectory(x, u, predicted_trajectory, dilated_traj, targets, ax, i)
+
+        utils.plot_map(width=width_init, height=height_init)
+        plt.axis("equal")
+        plt.grid(True)
+        plt.pause(0.0001)
+
+        if break_flag:
+            break
+
+    print("Done")
+    if show_animation:
+        for i in range(robot_num):
+            plt.plot(trajectory[0, i, :], trajectory[1, i, :], "-r")
+        plt.pause(0.0001)
+        plt.show()
        
 if __name__ == '__main__':
     # main1()
-    main()
+    main2()
 
     
