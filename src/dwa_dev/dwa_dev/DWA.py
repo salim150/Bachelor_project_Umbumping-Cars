Added full stoppage for each of the method at either the goal or at collision. Now when the car stops also the speed will be 0.0 for all subsequent timesteps. Also corrected the average I was doing for the speed, acceleration and steering. Now the marker for the acceleration and steering is the average of the usage over the trajectory duration averaged per robot.

src/dwa_dev/dwa_dev/DWA.py
@@ -534,11 +534,12 @@ class DWA_algorithm():
         self.dilated_traj[i] = LineString(zip(predicted_trajectory1[:, 0], predicted_trajectory1[:, 1])).buffer(dilation_factor, cap_style=3)
 
         # Collision check
-        u = self.check_collision(x, u, i)
-
         x1 = motion(x1, u1, dt)
         x[:, i] = x1
         u[:, i] = u1
+        
+        u, x = self.check_collision(x, u, i)
+        
         self.predicted_trajectory[i] = predicted_trajectory1
         self.u_hist[i] = u_history
         
@@ -664,6 +665,8 @@ class DWA_algorithm():
                 print("Collision detected")
                 self.reached_goal[i] = True
                 u[:, i] = np.zeros(2)
+                x[3, i] = 0
+
 
         for idx in range(self.robot_num):
             if idx == i:
@@ -675,7 +678,8 @@ class DWA_algorithm():
                     print("Collision detected")
                     self.reached_goal[i] = True
                     u[:, i] = np.zeros(2)
-        return u
+                    x[3, i] = 0
+        return u, x
 
 def main():
     """

src/lbp_dev/lbp_dev/LBP.py
@@ -459,6 +459,9 @@ def update_robot_state(x, u, dt, targets, dilated_traj, u_hist, predicted_trajec
     x1 = motion(x1, u1, dt)
     x[:, i] = x1
     u[:, i] = u1
+
+    # u, x = self.check_collision(x, u, i)
+
     predicted_trajectory[i] = predicted_trajectory1
     u_hist[i] = u_history
 
@@ -551,7 +554,9 @@ class LBP_algorithm():
                     t_prev = time.time()
                     x, u, self.predicted_trajectory, self.u_hist = update_robot_state(x, u, dt, self.targets, self.dilated_traj, self.u_hist, self.predicted_trajectory, i)
                     self.computational_time.append(time.time()-t_prev)
-                u = self.check_collision(x, u, i) 
+
+                u, x = self.check_collision(x, u, i) 
+            
             else:
                 print(u[:, i])
             # If we want the robot to disappear when it reaches the goal, indent one more time
@@ -581,6 +586,7 @@ class LBP_algorithm():
                 print("Collision detected")
                 self.reached_goal[i] = True
                 u[:, i] = np.zeros(2)
+                x[3, i] = 0
 
         for idx in range(self.robot_num):
             if idx == i:
@@ -592,8 +598,9 @@ class LBP_algorithm():
                     print("Collision detected")
                     self.reached_goal[i] = True
                     u[:, i] = np.zeros(2)
+                    x[3, i] = 0
         
-        return u
+        return u, x
 
 def main():
     """

src/seed_simulation/seed_simulation/data_process.py
@@ -26,6 +26,16 @@ class DataProcessor:
     def __init__(self, robot_num, file_name):
         self.robot_num = robot_num
         self.file_name = file_name
+    
+    def calculate_goal_reached_index(self, trajectory, i):
+        last_idx = 0
+        for idx in range(1, len(trajectory[4,i,:])):
+            idx_prev = idx-1
+            if trajectory[4, i, idx] == 0.0 and trajectory[5, i, idx] == 0.0 and idx > last_idx:
+                if trajectory[4, i, idx_prev] != 0.0 or trajectory[5, i, idx_prev] != 0.0:
+                    last_idx = idx
+        
+        return last_idx
 
     def calculate_path_length(self, trajectory, i):
         """
@@ -76,8 +86,9 @@ class DataProcessor:
         :param trajectory: the trajectory
         :return: the acceleration usage
         """
-        a = trajectory[4, i, :]
-        return np.sum(np.abs(a))
+        idx = self.calculate_goal_reached_index(trajectory, i)
+        a = trajectory[4, i, :idx+1]
+        return np.mean(np.abs(a))
     
     def calculate_avg_acceleration_usage(self, trajectory):
         """
@@ -97,8 +108,9 @@ class DataProcessor:
         :param trajectory: the trajectory
         :return: the steering usage
         """
-        steering = trajectory[5, i, :]
-        return np.sum(np.abs(steering))
+        idx = self.calculate_goal_reached_index(trajectory, i)
+        steering = trajectory[5, i, :idx+1]
+        return np.mean(np.abs(steering))
     
     def calculate_avg_steering_usage(self, trajectory):
         """
@@ -118,7 +130,8 @@ class DataProcessor:
         :param trajectory: the trajectory
         :return: the average speed
         """
-        v = trajectory[3, i, :]
+        idx = self.calculate_goal_reached_index(trajectory, i)
+        v = trajectory[3, i, :idx+1]
         return np.mean(v)
     
     def calculate_avg_speed(self, trajectory):